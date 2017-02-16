bcb10cfc2f78d4a72f4dcf0d059996b2e8b4d609
Jay <pateljay0323@gmail.com>
2017-02-17 00:31:43 +0530

    Added Take Appointment Activity

--- app/src/main/java/com/harsh/ecare/AppointmentActivity.java
+++ app/src/main/java/com/harsh/ecare/AppointmentActivity.java
@@ -1,20 +1,33 @@
 package com.harsh.ecare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class AppointmentActivity extends AppCompatActivity {
 
     ArrayList<Appointment> contacts;
+    private Button takeAppointment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
+
+        takeAppointment = (Button) findViewById(R.id.button5);
+        takeAppointment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TakeAppointmentActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // Lookup the recyclerview in activity layout
         RecyclerView rvContacts = (RecyclerView) findViewById(R.id.appointmentList);
