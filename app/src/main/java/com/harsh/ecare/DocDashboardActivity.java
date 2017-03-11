ccc52626813450389648700caf1c427ff78825c3
Jay <pateljay0323@gmail.com>
2017-03-11 12:13:21 +0530

    delete appointment on long press uncommented

--- app/src/main/java/com/harsh/ecare/DocDashboardActivity.java
+++ app/src/main/java/com/harsh/ecare/DocDashboardActivity.java
@@ -3,6 +3,8 @@ package com.harsh.ecare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -44,5 +46,28 @@ public class DocDashboardActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dashboard, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
